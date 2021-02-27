7e182d5f96c57e7607e3dd8d3fae2873432db270
bayard7523 <49534036+bayard7523@users.noreply.github.com>
2021-02-27 13:11:03 +0200

    fixed language

--- src/quasar.js
+++ src/quasar.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 
 import 'quasar/dist/quasar.css'
-import lang from 'quasar/lang/ s.js'
+import lang from 'quasar/lang/en-us.js'
 import '@quasar/extras/material-icons/material-icons.css'
 import { Quasar } from 'quasar'
 
